18e15232a7c0b932cb978c962beb79f2a31d9553
LingLambda <abc1514671906@163.com>
2025-04-07 09:02:48 +0800

    完成blog大致框架

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   //悬停预加载
   prefetch: true,
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
