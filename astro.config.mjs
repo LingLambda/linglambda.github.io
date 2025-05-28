7ad2b261fb08649bde14de44d015de763b838e6c
LingLambda <abc1514671906@163.com>
2025-05-28 10:07:01 +0800

    添加 sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
 import { defineConfig, envField } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://linglambda.github.io",
+  site: "https://blog.llma.top",
   base: "/",
 
   //悬停预加载
   prefetch: true,
 
   vite: {},
+
   env: {
     schema: {
       PATH_MODULE: envField.enum({
@@ -27,11 +30,18 @@ export default defineConfig({
       }),
     },
   },
+
   markdown: {
     shikiConfig: {
       // 从 Shiki 的内置主题中选择 (或者添加你自己的)
       // https://shiki.style/themes
-      theme: 'one-light',
-    }
-  }
+      theme: "one-light",
+    },
+  },
+
+  integrations: [
+    sitemap({
+      // sitemap配置
+    }),
+  ],
 });
